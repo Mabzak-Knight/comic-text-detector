7fa8bd7315be9ed52fc2b81146697906e00eb0fd
M. Agus Badruzaman Al Khoir <56875726+Mabzak-Knight@users.noreply.github.com>
2024-03-01 19:34:40 +0700

    fix error xyxy2yolo

--- utils/imgproc_utils.py
+++ utils/imgproc_utils.py
@@ -37,7 +37,7 @@ def xywh2xyxypoly(xywh, to_int=True):
     return xyxypoly
 
 def xyxy2yolo(xyxy, w: int, h: int):
-    if xyxy == [] or xyxy == np.array([]) or len(xyxy) == 0:
+    if xyxy is None or not np.any(xyxy):
         return None
     if isinstance(xyxy, list):
         xyxy = np.array(xyxy)
